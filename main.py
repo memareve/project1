d08723c1f4191184a8daff7450443ded2ffcbf22
memareve <yqqN87cjbeER>
2020-10-26 10:32:25 +0700

    done 2

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-area_states = 8080464.3  # without Islands, Alaska and Hawaii (48 states)
+area_states = 7830000  # without Islands, Alaska and Hawaii (48 states)
 volume_water = 22671  # volume of water in the great lakes
 print((volume_water/area_states) * 1000, 'metres')
 
